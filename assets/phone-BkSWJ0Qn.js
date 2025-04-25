915c76be48742eb331173a24ecfedc748d15cf05
pratikpsl <pratiklad9625@gmail.com>
2025-04-26 00:09:56 +0530

    Updates

--- assets/phone-BkSWJ0Qn.js
+++ assets/phone-BkSWJ0Qn.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-BdQhEDxZ.js";/**
+import{c as o}from"./index-ByGYXmVf.js";/**
  * @license lucide-react v0.503.0 - ISC
  *
  * This source code is licensed under the ISC license.
